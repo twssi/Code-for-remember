9fce37d1f5bf64e4962e515c65848ef658aca722
twssi <twssi@naver.com>
2025-01-10 17:32:51 +0900

    .

--- textgame/main.c
+++ textgame/main.c
@@ -1,6 +1,8 @@
 #include "main.h"
+#include "player.h"
+#include "monster.h"
 
-
+static int player_databuf[5]= {0,};
 
 
 int main(void)
@@ -12,35 +14,102 @@ int main(void)
 }
 
 
+void monster_data(void)
+{
 
-// 전역 변수 정의
-Player player;
-Monster monster;
-defaultitem playeritem = {.zealot = 5, .mage = 5};
-
-void monster_data(void) {
-    int monsterhealth = 30;
-    int monster_damage = 10;
-    int monster_armor = 20;
 }
 
-void player_data(int job) {
-    if(1 == job) {
-        player.health = player_zealot_default_health;
-        player.mana = player_zealot_default_mana;
-        player.armor = player_zealot_default_armor;
+void player_data(int job) 
+{
+    
+
+    if(1 == job) 
+    {
+        player.health = player_zealot_default_health,
+        player.mana = player_zealot_default_mana,
+        player.armor = player_zealot_default_armor,
         player.damage = player_zealot_default_damage;
+
+        player_databuf[0] = player.health;
+        player_databuf[1] = player.mana;
+        player_databuf[2] = player.armor;
+        player_databuf[3] = player.damage;
+        
+        battle_data(player.damage,  player.health);
+
     }
-    else if(2 == job) {
+
+    else if(2 == job) 
+    {
         player.health = player_mage_default_health;
         player.mana = player_mage_default_mana;
         player.armor = player_mage_default_armor;
         player.damage = player_mage_default_damage;
+
+        player_databuf[0] = player.health;
+        player_databuf[1] = player.mana;
+        player_databuf[2] = player.armor;
+        player_databuf[3] = player.damage;
+    }
+
+    else if(3 == job)
+    {
+        player.health = player_mercenary_default_health;
+        player.mana = player_mercenary_default_mana;
+        player.armor = player_mercenary_default_armor;
+        player.damage = player_mercenary_default_damage;
+        
+        player_databuf[0] = player.health;
+        player_databuf[1] = player.mana;
+        player_databuf[2] = player.armor;
+        player_databuf[3] = player.damage;
     }
     
+    else if(4 == job)
+    {
+        player.health = player_paladin_default_health;
+        player.mana = player_paladin_default_mana;
+        player.armor = player_paladin_default_armor;
+        player.damage = player_paladin_default_damage;
+        
+
+        player_databuf[0] = player.health;
+        player_databuf[1] = player.mana;
+        player_databuf[2] = player.armor;
+        player_databuf[3] = player.damage;
+    }
+
+    else if(5 == job)
+    {
+        player.health = player_rogue_default_health;
+        player.mana = player_rogue_default_mana;
+        player.armor = player_rogue_default_armor;
+        player.damage = player_rogue_default_damage;
+
+        player_databuf[0] = player.health;
+        player_databuf[1] = player.mana;
+        player_databuf[2] = player.armor;
+        player_databuf[3] = player.damage;
+    }
+
+    else if(6 == job)
+    {
+        player.health = player_inquisitor_default_health;
+        player.mana = player_inquisitor_mana;
+        player.armor = player_inquisitor_armor;
+        player.damage = player_inquisitor_damage;
+
+        player_databuf[0] = player.health;
+        player_databuf[1] = player.mana;
+        player_databuf[2] = player.armor;
+        player_databuf[3] = player.damage;
+    }
+
+
 }
 
-const char* player_job(int job) {
+const char* player_job(int job) 
+{
     if(1 == job) return "광신도";
     else if (2 == job) return "마법사";
     else if (3 == job) return "용병";
@@ -50,7 +119,8 @@ const char* player_job(int job) {
     return "알 수 없는 직업";
 }
 
-void characater_select(void) {
+void characater_select(void) 
+{
     unsigned int job;
     char answer = 'n';
    
@@ -86,8 +156,10 @@ void characater_select(void) {
     }
 }
 
-void intro(void) {
-    switch(player.job) {
+void intro(void)
+{
+    switch(player.job)
+    {
         case 1: //광신도
             printf("\n 맹목적인 신앙심에 모든것을 바친자.\n");
             printf("%s은 이 세계의 신앙속에서 홀로 일어선 선지자 혹은 이단자. 마을의 이단 심판에서도 살아남았다.\n", player.player_name);
@@ -95,23 +167,54 @@ void intro(void) {
             break;
         case 2: // 흑마법사
             printf("\n 어둠과 계약한 자.\n");
-            printf("%s은 영혼을 대가로 금단의 지식을 얻었으나, 대가는 영원한 마력과 생명에대한 갈증이었다.\n", player.player_name);
+            printf("%S은 영혼을 대가로 금단의 지식을 얻었으나, 대가는 영원한 마력과 생명에대한 갈증이었다.\n", player.player_name);
             printf("끝없는 영혼을 고치기위해 또 다시 금단의 지식을 찾아 왕국의 타락한 도시로 향한다.\n");
             break;
-        // ... 나머지 직업들의 intro
+        case 3: // 용병
+            printf("\n악랄한 돈의 전사.\n");
+            printf("%s은 고용한 고용주의 돈을 위하여 싸웠다. 하지만 당신을 고용한 고용주의 배신에의해 모든걸 빼앗겼다.\n", player.player_name);
+            printf("아무것도 남지 않은 당신은 타락한 왕국의 도시 속에서 또 다른 기회를 찾았다. \n");
+            break;
+        case 4: // 성기사
+            printf("\n성기사: 타락한 왕국의 성스러운 기사.\n");
+            printf("%s은 왕국을 수호하겠다는 신성한 다짐을 하였지만 거대한 타락의 기운을 당신 홀로 막아낼 순 없었다.\n", player.player_name);
+            printf("신성했던 도시의 모습과 왕국의 옛 영광을 재건하고 신의 뜻을 받들고자 당신은 타락의 기운이 나오는 도시로 홀로 향했다.\n");
+            break;
+        case 5: // 도적
+            printf("\n 가진것을 빼앗고 속이는 더러운 길거리의 무법자.\n");
+            printf("%s은 타락한 도시로 흘러가는 부랑자, 모험자들을 털어 재물을 챙기는 도시 속 쓰레기다.\n", player.player_name);
+            printf("이 도시를 찾아오는 어리숙한 부랑자들이 줄어들자 타락의 기운이 깃든 도시속 보물을 손에 넣을 생각 뿐이다.\n");
+            break;
+        case 6: // 이단심문관
+            printf("\n 신의 이름을 빌어 이단을 처단 하는자.\n");
+            printf("%s은 신의 이름 아래에서 모든 이단자를 처벌하기로 맹세한 당신은 신의 이름아래 이단자와 마녀라는 사람들을 처단해왔다.\n", player.player_name);
+            printf("이 타락한 도시 속 이단의 근원을 찾아 없애기 위해 스스로 타락한 도시속으로 향한다.\n");
+            break;
+   
+        default:
+            printf("\n알 수 없는 직업입니다.\n");
+            break;
     }
 
-    printf("도시로 향한 %s은 수십 수일의 시간이 흘러 도시 앞 그 누구도 지키고 있지 않은 문 앞에 당도 하였다.\n", player.player_name);
+    printf("도시로 향한 %s은 수십 수일의 시간이 흘러 도시 앞 그 누구도 지키고 있지 않은 문 앞에 당도 하였다.\n");
     printf("\n어둡고 적막한 도시의 모습만이 당신을 기다리고 있다. 무엇도 장담 할 수 없는 어두운 그곳에서 각자의 이야기를 따라 흘러가며\n");
     printf("타락한곳에서 모든것이 하나로 모이니.\n");
     printf("그대는...준비되었는가?\n");
 }
+void battle_data(Player player, Monster monster)
+{
 
-void battle_data(int damage, int health) {
-    // 전투 데이터 처리
+
+    if(player.health >0 && monster.health)
+    {
+        printf("적을 만났다! \n");
+
+
+    }
 }
 
-void battle_info(void) {
+void battle_info(void)
+{
     printf("당신은 기습 당했습니다! 전투를 준비하십시오!\n");
     printf("미처 판단하기도 전에 당신은 죽었습니다...\n");
 }
---
+++ textgame/monster.h
@@ -0,0 +1,7 @@
+
+//몬스터
+
+#define monster_weekbone_health             15
+#define monster_weekbone_armor              0
+#define monster_weekbone_damage             7
+#define monster_weekbone_mana               0
